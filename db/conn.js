80e14e79cc32df8fe81b50a4fd96777b5a68cc12
faizan-ahmed-147 <fa147672@gmail.com>
2023-02-01 12:37:48 +0500

    first commit

--- db/conn.js
+++ db/conn.js
@@ -2,6 +2,7 @@ const mongoose = require ('mongoose');
 const dotenv = require("dotenv")
 dotenv.config({ path: './config.env'})
 const mongoURI = process.env.DATABASE;
+mongoose.set('strictQuery', false);
 const connectToMongo = async()=>{
     await mongoose.connect(mongoURI, ()=>{
         console.log("Connect To Mongo Successfully");

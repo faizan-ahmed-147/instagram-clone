631e0185ca074290edd1744c11018eec888596ff
faizan-ahmed-147 <fa147672@gmail.com>
2023-02-01 12:07:40 +0500

    db

--- db/conn.js
+++ db/conn.js
@@ -2,8 +2,8 @@ const mongoose = require ('mongoose');
 const dotenv = require("dotenv")
 dotenv.config({ path: './config.env'})
 const mongoURI = process.env.DATABASE;
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
+const connectToMongo = async()=>{
+    await mongoose.connect(mongoURI, ()=>{
         console.log("Connect To Mongo Successfully");
     })
 }
